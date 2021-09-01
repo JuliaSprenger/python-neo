6b77157e4007ea5c48d3909343fb9d4ecf5a45a9
sprenger <julia.sprenger@univ-amu.fr>
2021-09-01 15:37:43 +0200

    [debug] add more prints for astype investigation

--- neo/rawio/neuralynxrawio/ncssections.py
+++ neo/rawio/neuralynxrawio/ncssections.py
@@ -287,10 +287,10 @@ class NcsSectionsFactory:
         gap_rec_ids = list(np.where(ncsMemMap['nb_valid'] != exp_nb_valid)[0])
 
         pred_times = np.rint(ncsMemMap['timestamp'] + 1e6 / ncsSects.sampFreqUsed * ncsMemMap['nb_valid']).astype(int)
-        print(f'numpy version: {np.version}')
-        print(f'pred_times: {pred_times}')
-        print(f"non-rounded pred_times: {ncsMemMap['timestamp'] + 1e6 / ncsSects.sampFreqUsed * ncsMemMap['nb_valid']}")
-        print(f"tmp pred_times: {np.rint(ncsMemMap['timestamp'] + 1e6 / ncsSects.sampFreqUsed * ncsMemMap['nb_valid'])}")
+        print(f'pred_times type: {pred_times.dtype}')
+        print(f'numpy version: {np.__version__}')
+        print(f'astyped pred_times: {pred_times}')
+        print(f"rounded pred_times: {np.rint(ncsMemMap['timestamp'] + 1e6 / ncsSects.sampFreqUsed * ncsMemMap['nb_valid'])}")
         max_pred_times = pred_times + maxGapLen
         # data records that start later than the predicted time (including the
         # maximal accepted gap length) are considered delayed a gap is
